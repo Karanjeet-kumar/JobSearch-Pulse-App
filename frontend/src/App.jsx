dd5c18db0ddb97013ad98be32f07158ac3e091eb
Karanjeet-kumar <karanrajput8400@gmail.com>
2024-12-11 00:07:11 +0530

    JobPage-Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import Home from "./components/HomePage";
+import JobPage from "./components/JobPage";
 
 const appRouter = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ const appRouter = createBrowserRouter([
     path: "/signup",
     element: <Signup />,
   },
+  {
+    path: "/jobs",
+    element: <JobPage />,
+  }
 ]);
 
 function App() {
