cdd3a4202e3adef2f0bfb398d2981957c9fbe513
Karanjeet-kumar <karanrajput8400@gmail.com>
2025-01-26 01:21:17 +0530

    Implement Browse Search Job Logic

--- frontend/src/components/hooks/useGetAllJobs.jsx
+++ frontend/src/components/hooks/useGetAllJobs.jsx
@@ -1,18 +1,22 @@
 import { setAllJobs } from "../redux/jobSlice";
 import axios from "axios";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { JOB_API_END_POINT } from "../utils/api_constant";
 
 const useGetAllJobs = () => {
   const dispatch = useDispatch();
+  const { searchedQuery } = useSelector((store) => store.job);
   useEffect(() => {
     const fetchAllJobs = async () => {
       // API connected to Frontend(GetAllJobs)
       try {
-        const res = await axios.get(`${JOB_API_END_POINT}/get`, {
-          withCredentials: true,
-        });
+        const res = await axios.get(
+          `${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,
+          {
+            withCredentials: true,
+          }
+        );
         if (res.data.success) {
           dispatch(setAllJobs(res.data.jobs));
         }
